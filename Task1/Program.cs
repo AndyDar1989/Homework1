53395d898f65fec0f1b914c6b7bcf408f0522195
AndrewDar <geologalone@gmail.com>
2022-08-04 14:46:43 +0300

    added programm for max number

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3*/
+
+Console.Write("Enter first number: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter second number: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+if (firstNumber>secondNumber)
+{
+    Console.WriteLine($"Max= {firstNumber}");
+}
+else 
+{
+    Console.WriteLine($"Max= {secondNumber}");
+}
