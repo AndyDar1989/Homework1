1451a20ee275a659d8e2ea0f21a95ec764c26153
AndrewDar <geologalone@gmail.com>
2022-08-04 16:20:11 +0300

    added program for task6

---
+++ Task6/Program.cs
@@ -0,0 +1,8 @@
+﻿/*Задача 7: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+456 -> 6
+782 -> 2
+918 -> 8*/
+Console.Write("Enter three-digit number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int lastNumber = number%10;
+Console.Write($"Last number is {lastNumber}");
