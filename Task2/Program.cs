7495095a6d37324da542e0d33278702143dc07aa
AndrewDar <geologalone@gmail.com>
2022-08-06 11:16:13 +0300

    added second decision

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -2,15 +2,7 @@
 2, 3, 7 -> 7
 44 5 78 -> 78
 22 3 9 -> 22*/
-
-Console.Write("Enter first number: ");
-int firstNumber = Convert.ToInt32(Console.ReadLine());
-Console.Write("Enter second number: ");
-int secondNumber = Convert.ToInt32(Console.ReadLine());
-Console.Write("Enter third number: ");
-int thirdNumber = Convert.ToInt32(Console.ReadLine());
-
-if ( firstNumber>secondNumber && firstNumber>thirdNumber)
+/*if ( firstNumber>secondNumber && firstNumber>thirdNumber)
 {
     Console.WriteLine($"Maximal number is {firstNumber}");
 }
@@ -18,7 +10,17 @@ else if (secondNumber>firstNumber && secondNumber>thirdNumber)
 {
     Console.WriteLine($"Maximal number is {secondNumber}");
 }
-else
-{
-   Console.WriteLine($"Maximal number is {thirdNumber}"); 
-}
+else*/
+
+Console.Write("Enter first number: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter second number: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter third number: ");
+int thirdNumber = Convert.ToInt32(Console.ReadLine());
+int max = firstNumber;
+if ( secondNumber>max)
+   max = secondNumber;
+if (thirdNumber>max)
+    max = thirdNumber;
+Console.WriteLine($"Maximal number is {max}"); 
